88cbd466a5befc7f801601c037e1a919f80855c1
Shivam Patel <patelsd96@gmail.com>
2020-05-14 18:39:14 -0400

    added sidebar template

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import About from './components/about'
+import Sidebar from './components/sidebar'
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
+          <Sidebar></Sidebar> 
           <div id="colorlib-main">
             <About></About>
           </div>
