d7b997a2d5eb1131069049f74ef1d9d1aca3d4a9
Shivam Patel <patelsd96@gmail.com>
2020-05-15 19:19:07 -0400

    finalized and published to github pages

---
+++ build/precache-manifest.7f23ed2c4d794705c1a9dc65dc3dae27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90912ff46ff5ebf4acc0",
+    "url": "/ReactPortfolio/static/js/runtime~main.90912ff4.js"
+  },
+  {
+    "revision": "dc79d3e8c8b18fce833f",
+    "url": "/ReactPortfolio/static/js/main.dc79d3e8.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/ReactPortfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "dc79d3e8c8b18fce833f",
+    "url": "/ReactPortfolio/static/css/main.9f287c23.chunk.css"
+  },
+  {
+    "revision": "ee9c7d8ecd47d5114f7df59d9f892148",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
