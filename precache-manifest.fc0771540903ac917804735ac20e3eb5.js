24ea7551106a85bde1b8e24daa10b44dc94038bb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-16 00:54:31 +0000

    Deploy ShivamP96/ReactPortfolio to github.com/ShivamP96/ReactPortfolio.git:gh-pages

---
+++ precache-manifest.fc0771540903ac917804735ac20e3eb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c1abad76c6b988a9e38",
+    "url": "/ReactPortfolio/static/css/main.55a028ee.chunk.css"
+  },
+  {
+    "revision": "2c1abad76c6b988a9e38",
+    "url": "/ReactPortfolio/static/js/main.2c1abad7.chunk.js"
+  },
+  {
+    "revision": "90912ff46ff5ebf4acc0",
+    "url": "/ReactPortfolio/static/js/runtime~main.90912ff4.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/ReactPortfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "329d776b6a463771ef7de569bb3a180f",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
